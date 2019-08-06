a09f1b89a3f81b9ec97220f61c5f22f6089690e1
Gabriel <jgj_junior@yahoo.com.br>
2019-08-05 21:26:48 -0300

    final front end

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			route: "signin"
+			route: "signin",
+			isSignedIn: false
 		};
 	}
 	routeChange = route => {
+		if(route === "signin") {
+			this.setState({isSignedIn:true})
+		}
 		this.setState({ route });
 	};
 	render() {
 		return (
 			<div>
-				<Navbar />
+				<Navbar route={this.state.route} />
 				<Content routeChange={this.routeChange} route={this.state.route} />
 				<Button onClick={() => this.routeChange("home")}>Home</Button>
 				<Button onClick={() => this.routeChange("signin")}>Sign in</Button>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Nav, NavLink } from "reactstrap";
 
-export default function Navbar() {
-	return (
-		<div>
-			<Nav>
-				<NavLink href="#">Sign out</NavLink>
-			</Nav>
-			<hr />
-		</div>
-	);
+export default function Navbar({ route }) {
+	if (route === "home") {
+		return (
+			<div>
+				<Nav>
+					<NavLink href="#">Sign out</NavLink>
+				</Nav>
+				<hr />
+			</div>
+		);
+	} else
+		return (
+			<div>
+				<Nav>
+					<NavLink href="#">Register</NavLink>
+					<NavLink href="#">About</NavLink>
+				</Nav>
+				<hr />
+			</div>
+		);
 }
